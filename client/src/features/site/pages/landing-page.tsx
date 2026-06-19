333c3bb2006a370713817fb5d14087922686b7ae
Marion Braide <peaceibinabobraide@gmail.com>
2026-06-19 02:04:24 +0100

    feat(site): build value proposition section for landing page

--- client/src/features/site/pages/landing-page.tsx
+++ client/src/features/site/pages/landing-page.tsx
@@ -1,11 +1,13 @@
 import { AboutSection } from '#/features/site/components/about'
 import { HeroSection } from '#/features/site/components/hero'
+import { ValuePropositionSection } from '#/features/site/components/value-proposition'
 
 export function LandingPage() {
   return (
     <div>
       <HeroSection />
       <AboutSection />
+      <ValuePropositionSection />
     </div>
   )
 }
