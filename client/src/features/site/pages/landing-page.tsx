f641ff8f0b55c88a5694a5b43c4dba1dc9ae981b
Marion Braide <peaceibinabobraide@gmail.com>
2026-06-19 21:32:54 +0100

    feat(site): build what we offer section for landing page without remaining images for interaction

--- client/src/features/site/pages/landing-page.tsx
+++ client/src/features/site/pages/landing-page.tsx
@@ -3,6 +3,8 @@ import { HeroSection } from '#/features/site/components/hero'
 import { MissionSection } from '#/features/site/components/mission'
 import { ValuePropositionSection } from '#/features/site/components/value-proposition'
 
+import { WhatWeOfferSection } from '../components/what-we-offer'
+
 export function LandingPage() {
   return (
     <div>
@@ -10,6 +12,7 @@ export function LandingPage() {
       <AboutSection />
       <ValuePropositionSection />
       <MissionSection />
+      <WhatWeOfferSection />
     </div>
   )
 }
