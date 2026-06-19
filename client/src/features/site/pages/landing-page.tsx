947647866b585564df219174429ee9b3c7bf327c
Marion Braide <peaceibinabobraide@gmail.com>
2026-06-19 01:40:18 +0100

    feat(site): build about section for landing page

--- client/src/features/site/pages/landing-page.tsx
+++ client/src/features/site/pages/landing-page.tsx
@@ -1,9 +1,11 @@
+import { AboutSection } from '#/features/site/components/about'
 import { HeroSection } from '#/features/site/components/hero'
 
 export function LandingPage() {
   return (
     <div>
       <HeroSection />
+      <AboutSection />
     </div>
   )
 }
